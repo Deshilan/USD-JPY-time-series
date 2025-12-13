9aa1d64d14d637ca97da80a04bfcc5bd56471527
Deshilan <janakadeshi@gmail.com>
2025-12-13 01:39:53 +0100

    data importers

---
+++ Data_import.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+def data():
+    data = pd.read_csv('USD_JPY_2015_07_2025_ASK.csv')
+    data.set_index('timestamp', inplace=True)
+    return data
+
+def cleared_data():
+    data = pd.read_csv('USD_JPY_2015_07_2025_ASK.csv')
+    data.set_index('timestamp', inplace=True)
+    data.drop(columns=['volume','Open','High','Low'], inplace=True)
+    return data
+
+def one_day1(start):
+    data = cleared_data()
+    day = data.Close.values[(start*1440): (start*1440 + 1440)]
+
+one_day1(5)
